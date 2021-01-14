ecab022c1db3335c4571c73ccd48674fc655a2f4
julietezekwe <julietezekwe@gmail.com>
2021-01-14 20:44:15 +0100

    fix redis connection

--- src/helpers/redis.ts
+++ src/helpers/redis.ts
@@ -6,7 +6,8 @@ const Redis: any = promisifyAll(redis);
 
 const redisClient: any = Redis.createClient({
   host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT)
+  port: Number(process.env.REDIS_PORT),
+  no_ready_check: true,
 });
 
 process.on("SIGINT", () => redisClient.quit());
